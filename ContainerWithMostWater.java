764072d22031fb7390e80774aa1a4eddfadeb4e3
EnHuix <952927520@qq.com>
2018-03-09 20:04:35 +0800

    ThreeQuestion

---
+++ ContainerWithMostWater.java
@@ -0,0 +1,43 @@
+/*
+Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai).
+n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). 
+Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+Note: You may not slant the container and n is at least 2.
+
+
+class Solution {
+    public int maxArea(int[] height) {
+        int max=0;
+        int res;
+        for(int i=0;i<height.length;i++){
+            for(int j=i+1;j<height.length;j++){
+                res=((height[i]<height[j])?height[i]:height[j])*(j-i);
+                if(max<res)
+                    max=res;
+            }
+        }
+        return max;
+    }
+}
+
+
+以上时间复杂度为O(n^2)
+*/
+
+class Solution {
+    public int maxArea(int[] height) {
+        int max=0;
+        int l=0,r=height.length-1;
+        while(l<r){
+            max=Math.max(max,((height[l]<height[r])?height[l]:height[r])*(r-l));
+            if(height[l]<=height[r])
+                l++;
+            else
+                r--;
+        }
+        return max;
+    }
+}
+
+//该方法时间复杂度为O(n),空间复杂度为O(1).
---
+++ LongestCommonPrefix.java
@@ -0,0 +1,34 @@
+//Write a function to find the longest common prefix string amongst an array of strings.
+//注意substring的用法
+/*
+public String substring(int beginIndex, int endIndex)
+第一个int为开始的索引，对应String数字中的开始位置，
+第二个是截止的索引位置，对应String中的结束位置
+1、取得的字符串长度为：endIndex - beginIndex;
+2、从beginIndex开始取，到endIndex结束，从0开始数，其中不包括endIndex位置的字符
+如：
+"hamburger".substring(4, 8) returns "urge"
+ "smiles".substring(1, 5) returns "mile"public String substring(int beginIndex, int endIndex)
+第一个int为开始的索引，对应String数字中的开始位置，
+第二个是截止的索引位置，对应String中的结束位置
+1、取得的字符串长度为：endIndex - beginIndex;
+2、从beginIndex开始取，到endIndex结束，从0开始数，其中不包括endIndex位置的字符
+如：
+"hamburger".substring(4, 8) returns "urge"
+ "smiles".substring(1, 5) returns "mile"
+*/
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+     if(strs.length==0)
+         return "";
+        String res=strs[0];
+        for(int i=0;i<strs.length;i++){
+            while(strs[i].indexOf(res)!=0){
+                res=res.substring(0,res.length()-1);
+                if(res.isEmpty())
+                    return res;
+            }
+        }
+        return res;
+    }
+}
