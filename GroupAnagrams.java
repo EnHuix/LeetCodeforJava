4ef83d3022af076e23853598112bd5a01283dd93
EnHuix <952927520@qq.com>
2018-03-18 13:44:57 +0800

    给定字符串数组，将相同组成的放在一个数组里，返回所有分组

---
+++ GroupAnagrams.java
@@ -0,0 +1,29 @@
+/*
+Given an array of strings, group anagrams together.
+
+For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"], 
+Return:
+
+[
+  ["ate", "eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+
+*/
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> res=new HashMap<String,List<String>>();
+        for(String str:strs){
+            char[] a=str.toCharArray();
+            Array.sort(a);
+            String keyStr=String.valueOf(a);
+            if(!res.containsKey(keyStr))
+                res.put(keyStr,new ArrayList<String>());
+            res.get(keyStr).add(str);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+  
+}
