38a0070a880ad57c89facce29a32840b892e3509
EnHui <952927520@qq.com>
2018-03-18 20:37:30 +0800

    求字符串数组中各个字符串最大的公共部分

--- String/LongestCommonPrefix.java
+++ String/LongestCommonPrefix.java
@@ -25,10 +25,10 @@ class Solution {
         for(int i=0;i<strs.length;i++){
             while(strs[i].indexOf(res)!=0){
                 res=res.substring(0,res.length()-1);
-                if(res.isEmpty())
+                if(res.isEmpty()) 
                     return res;
             }
         }
-        return res;
+        return res; 
     }
 } 
